910bde358af62178679233dae8244db2d091277b
Juan Carlos Villaseñor-Derbez <jc_villasenor@miami.edu>
2025-02-17 20:40:07 -0500

    Added scripts

--- scripts/02_processing/01_clean_and_score_tracks.R
+++ scripts/02_processing/01_clean_and_score_tracks.R
@@ -9,26 +9,28 @@
 library(here)
 library(tidyverse)
 
-raw_tracks <- readRDS( here("data", "raw_data", "raw_tracks.rds")) %>% 
-  drop_na(course, speed)
+raw_tracks <- readRDS(here("data", "raw_data", "raw_tracks.rds")) |> 
+  drop_na(implied_speed_knots)
 
-mat <- raw_tracks %>% 
-  group_by(vessel_rnpa) %>% 
-  mutate(course_dif = c(0, diff(course))) %>% 
-  ungroup() %>% 
-  # filter(speed > 0) %>% 
-  select(speed) %>% 
+mat <- raw_tracks |> 
+  select(implied_speed_knots) |> 
   as.matrix()
   
 k_means <- kmeans(x = mat, centers = 2, nstart = 10)
 
-scored <- raw_tracks %>% 
+scored <- raw_tracks |> 
   mutate(inside = (between(lon, -115.471415, -110.078093) & between(lat, 17.655231, 20.008631)),
          before = datetime < "2017-11-27",
          before = ifelse(is.na(before) & ((year >= 2018) | (year == 2017& month >= 11)), F, before),
-         before = ifelse(is.na(before) & (year <= 2016 | year <= 2017 & month <= 10), T, before)) %>% 
+         before = ifelse(is.na(before) & (year <= 2016 | year <= 2017 & month <= 10), T, before)) |> 
   mutate(kmeans_fishing = fitted(k_means, "class") == 2,
-         speed_fishing = between(speed, 1, 12))
+         speed_fishing = between(implied_speed_knots, 1, 12)) |> 
+  arrange(kmeans_fishing)
+
+ggplot(scored |> filter(before, inside, year == 2015)) +
+  geom_point(aes(x = lon, y = lat, color = kmeans_fishing), pch = ".") +
+  coord_equal() +
+  facet_grid()
 
 saveRDS(object = scored,
         file = here("data", "processed_data", "scored_tracks.rds"))
--- scripts/04_content/03_near_ones.R
+++ scripts/04_content/03_near_ones.R
@@ -12,7 +12,7 @@ ports <- st_read(here("data", "processed_data", "ports.gpkg"))
 new_revilla <- st_read(here("data", "processed_data", "revilla_new.gpkg"))
 
 dist_to_mpa <- ports %>% 
-  mutate(distance = st_distance(geom, new_revilla, by_element = T)) %>% 
+  mutate(distance = st_distance(geom, new_revilla)) %>% 
   st_drop_geometry() %>% 
   units::drop_units()
 
@@ -39,7 +39,20 @@ dist_and_usage <- ggplot(data = disp) +
   labs(x = "Distance from home-port to MPA border (km)",
        y = "Percent of total effort within MPA") +
   theme_bw() +
-  theme(legend.position = "None")
+  theme(legend.position = "bottom")
+
+ggplot(data = disp,
+       mapping = aes(x = distance / 1e3, y = pct, fill = eu_rnpa)) +
+  geom_jitter(height = 0, width = 20,
+              shape = 21, color = "black") +
+  geom_point(pch = ".") +
+  geom_smooth(mapping = aes(x = distance / 1e3, y = pct),
+              formula = y ~  I(x ^ -1), color = "red", method = "lm", inherit.aes = F) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x = "Distance from home-port to MPA border (km)",
+       y = "Percent of total effort within MPA") +
+  theme_bw() +
+  theme(legend.position = "bottom")
 
 
 ggsave(plot = dist_and_usage,
@@ -64,7 +77,6 @@ vessel_info <- scored %>%
 kernell <- ggplot(data = closest,
        mapping = aes(x = pct, y = id, fill = eu_rnpa)) +
   geom_density_ridges(bandwidth = 0.009, alpha = 0.5) +
-  geom_vline(xintercept = 0.023, linetype = "dashed") +
   coord_flip() +
   scale_x_continuous(labels = scales::percent) +
   theme_bw() +
